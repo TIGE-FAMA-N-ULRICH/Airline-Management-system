d630852ef55d87c844eef9f94956e94ba2883cbd
TIGE-FAMA-N-ULRICH <119105998+TIGE-FAMA-N-ULRICH@users.noreply.github.com>
2024-04-28 13:46:24 +0100

    Update Staff_Airline.sql

--- DB/Staff_Airline.sql
+++ DB/Staff_Airline.sql
@@ -19,30 +19,6 @@ COMMIT;
 
 
 
-CREATE TABLE `Fights` (
-  `flight_id` int(11) NOT NULL,
-  `plane_id` int(11) NOT NULL,
-  `airline` varchar(255) NOT NULL,
-  `flight_number` varchar(255) NOT NULL,
-  `departure_airport` varchar(255) NOT NULL,
-  `arrival_airport` varchar(255) NOT NULL,
-  `departure_datetime` datetime NOT NULL,
-  `arrival_datetime` datetime NOT NULL,
-  `flight_duration` time DEFAULT NULL,
-  `aircraft_type` varchar(255) DEFAULT NULL,
-  `flight_status` varchar(50) DEFAULT NULL,
-  `ticket_price` decimal(10,2) DEFAULT NULL,
-  `available_seats` int(11) DEFAULT NULL,
-  `stopover_info` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `Fights`
-  ADD PRIMARY KEY (`flight_id`);
-
-ALTER TABLE `Fights`
-  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 
 
@@ -68,9 +44,53 @@ CREATE TABLE `Rental_planes` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Rental_planes`
+ALTER TABLE `Rental_plane`
   ADD PRIMARY KEY (`rental_id`);
 
 ALTER TABLE `Rental_planes`
   MODIFY `rental_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+
+
+
+CREATE TABLE Commercial_plane (
+    plane_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    manufacturer VARCHAR(255) NOT NULL,
+    year_of_manufacture int(11) DEFAULT NULL,
+    model VARCHAR(255) NOT NULL,
+    registration_number VARCHAR(50) DEFAULT NULL,  
+    maximum_capacity INT(11) NOT NULL,
+    availability_status ENUM('available', 'booked', 'maintenance') DEFAULT 'available',
+    maintenance_history TEXT DEFAULT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `Fights` (
+  `flight_id` int(11) NOT NULL,
+  `plane_id` int(11) NOT NULL,
+  `airline` varchar(255) NOT NULL,
+  `flight_number` varchar(255) NOT NULL,
+  `departure_airport` varchar(255) NOT NULL,
+  `arrival_airport` varchar(255) NOT NULL,
+  `departure_datetime` datetime NOT NULL,
+  `arrival_datetime` datetime NOT NULL,
+  `flight_duration` time DEFAULT NULL,
+  `aircraft_type` varchar(255) DEFAULT NULL,
+  `flight_status` varchar(50) DEFAULT NULL,
+  `ticket_price` decimal(10,2) DEFAULT NULL,
+  `available_seats` int(11) DEFAULT NULL,
+  `stopover_info` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+   FOREIGN KEY (`plane_id`) REFERENCES `Commercial_plane`(`plane_id`);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `Fights`
+  ADD PRIMARY KEY (`flight_id`);
+
+ALTER TABLE `Fights`
+  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
