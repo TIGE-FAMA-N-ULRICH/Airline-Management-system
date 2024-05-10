d65df8f9fdaea254186240513d526d7f645a4c4e
Sunnoogo77 <sunnoogo-caleb.tapsoba@efrei.net>
2024-05-10 09:51:55 +0100

    Update home.php

--- PHP/home.php
+++ PHP/home.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include 'Base.php';
+global $base;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +10,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../image/aircraft__1_-removebg-preview.png" type="image/x-icon">
     <title>Home</title>
-    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="../css/home.css">
     <script src="../js/javascript.js"></script>
 </head>
 <body>
     <header>
         <nav class="navbar">
             <div class="container">
-              <!--img src="../Img_logo/aircraft-removebg-preview.png" alt="Airline Management Logo" class="logo"-->
+              <img src="../image/aircraft-removebg-preview.png" alt="Airline Management Logo" class="logo">
               <ul class="nav-links">
                 <li><a href="home.php">Search Flights</a></li>
-                <li><a href="plane_rental_homepage.php">Rent a Plane</a></li>
-                <li><a href="#">About Us</a></li>
+                <li><a href="aircraft.php">Plan Rental</a></li>
+                <li><a href="aboutUs.php">About Us</a></li>
                 <?php
                 if(isset($_SESSION['user_id'])){
                   echo "<li><a href=\"\">Profile</a></li>";
@@ -37,10 +42,10 @@
     </header>
     <section class="section">
         <div class="content">
-          <p id="titre">The Sky is Waiting for You</p>
-          <p>With STAFFS_AIRWAYS, you can easily book any ticket you need to travel safely </p>
-          <p>thanks to our detailed system of searching and booking airline tickets.</p>
+        <p id="titre">The Sky is Waiting for You</p>
+        <p id="paragraphe">With "Staffs Airways", you can easily book any ticket you need to travel safely thanks to our detailed system of searching and booking airline tickets.</p>
         </div>
+
         <div class="category-buttons">
             <button class="category-button active" data-target="book-flight-form">Book Flight</button>
             <button class="category-button" data-target="rental-plane-form">Rental Plane</button>
@@ -48,8 +53,8 @@
         <div class="form-container">
             <div class="booking-form show" id="book-flight-form">
               <h2>Book a Flight</h2>
-              <form action="#" method="post" id="flight-form">
-                  <input type="checkbox" id="one-way" name="trip-type" value="one-way" checked>
+              <form action="Flight.php" method="post" id="flight-form">
+                  <input type="checkbox" id="one-way" name="trip-type" value="one-way" >
                   <label for="one-way">One Way</label><br><br>
                   <div class="row">
                       <div class="col">
@@ -82,41 +87,41 @@
                       <div class="col">
                           <label for="passengers">Number of Passengers:</label>
                           <div class="passenger-select">
-                              <input type="text" id="passengers" name="passengers" value="1" readonly>
+                              <input type="text" id="passengers" name="passengers" value="1" >
                               <div class="arrow-up"></div>
                               <div class="arrow-down"></div>
                           </div>
                       </div>
                   </div>
-                  <button type="submit">Search Flight</button>
+                  <button id="validate section " type="submit">Search Flight</button>
               </form>
             </div>
             <div class="rental-form" id="rental-plane-form">
                 <h2>Rent a Plane</h2>
                 <form action="#" method="post">
-
-
+                <div class="row">
+                      <div class="col">
+                          <label for="departure-city">From</label>
+                          <input type="text" id="departure-city" name="departure-city" required>
+                      </div>
+                      <div class="col">
+                          <label for="destination-city">To</label>
+                          <input type="text" id="destination-city" name="destination-city" required>
+                      </div>
+                  </div>
                     <div class="row">
-                        <div class="col">
+                      <div class="col">
                             <label for="depart">Departure Date:</label>
                             <input type="date" id="depart" name="depart" required>
                         </div>
                         <div class="col">
-                            <label for="return">Return Date:</label>
-                            <input type="date" id="return" name="return">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <label for="class">Plane Type</label>
-                            <select id="class" name="class">
-                                <option value="economy">Private Jet</option>
-                                <option value="business">Business Jet</option>
-                            </select>
-                        </div>
-                        <div class="col">
-
-                        </div>
+                          <label for="passengers">Number of Passengers:</label>
+                          <div class="passenger-select">
+                              <input type="text" id="passengers" name="passengers" value="1" >
+                              <div class="arrow-up"></div>
+                              <div class="arrow-down"></div>
+                          </div>
+                      </div>
                     </div>
                     <button type="submit">Book Now</button>
                 </form>
@@ -134,76 +139,76 @@
 
         <div class="features-grid">
             <div class="feature">
-                <img src="../Img_logo/diamond-removebg-preview.png" alt="Luxury and Comfort">
+                <img src="../image/diamond-removebg-preview.png" alt="Luxury and Comfort">
                 <h3>Luxury And Comfort</h3>
                 <p>Experience the pinnacle of luxury and comfort with our airline, where every journey is an exquisite and pleasurable experience, meticulously crafted to exceed your expectations.</p>
             </div>
 
             <div class="feature">
-                <img src="../Img_logo/avion-removebg-preview.png" alt="Save and Secure">
+                <img src="../image/avion-removebg-preview.png" alt="Save and Secure">
                 <h3>Save And Secure</h3>
                 <p>Travel with peace of mind knowing that our airline provides a safe and secure service, prioritizing your well-being every step of the way.</p>
             </div>
 
             <div class="feature">
-                <img src="../Img_logo/1-removebg-preview.png " alt="All Over The World">
+                <img src="../image/1-removebg-preview.png " alt="All Over The World">
                 <h3>All Over The World</h3>
                 <p>Discover the world at your fingertips with our airline, providing our customers the unparalleled opportunity to travel all over the globe.</p>
             </div>
         </div>
     </section>
     <section class="about-us">
-      <div class="about">
-        <div class="image-container">
-          <img src="../Img/about.jpeg" alt="About Us Image">
-        </div>
-        <div class="content">
-          <h2>About Us</h2>
-          <h1>Making Your Dreams Come True</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ligula ac ex tincidunt rutrum. Pellentesque euismod lorem vitae magna tincidunt tincidunt vel vel sapien.</p>
-          <button>Discover More</button>
-        </div>
-      </div>
-    </section>
+  <div class="about">
+    <div class="image-container">
+      <img src="../image/about.jpeg" alt="About Us Image">
+    </div>
+    <div class="content">
+      <h2>About Us</h2>
+      <h1>Making Your Dreams Come True</h1>
+      <p>Staffs Airways offers a unique and memorable travel experience, committed to service excellence, passenger comfort, and environmental sustainability. Our private jet charter services cater to discerning clients, providing luxurious and convenient travel options for business or special occasions.</p>
+      <a href="../php/aboutUs.php"><button>Discover More</button></a>
+    </div>
+  </div>
+</section>
 <section class="services">
     <h2>SERVICES</h2>
     <h3>What We Offer</h3>
-    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    <p>We offer a wide range of services tailored to your specific needs, ensuring exceptional customer experience and complete satisfaction.</p>
 
     <div class="services-grid">
         <div class="service">
-            <img src="../Img_logo/luxury.png" alt="Luxury and Comfort">
+            <img src="../image/luxury.png" alt="Luxury and Comfort">
             <h3>Luxury Travel</h3>
             <p>Experience the pinnacle of luxury and comfort with our airline, where every journey is an exquisite and pleasurable experience, meticulously crafted to exceed your expectations.</p>
         </div>
 
         <div class="service">
-            <img src="../Img_logo/work-schedule.png" alt="Save and Secure">
+            <img src="../image/work-schedule.png" alt="Save and Secure">
             <h3>Flexible Schedule</h3>
             <p>Travel at your own pace with our airline, offering our customers the freedom to choose a flexible schedule that fits their needs.</p>
         </div>
 
         <div class="service">
-            <img src="../Img_logo/low-price.png" alt="All Over The World">
+            <img src="../image/low-price.png" alt="All Over The World">
             <h3>Affordable Cost</h3>
             <p>Traveling affordably is our promise. Enjoy the opportunity to explore the world with our airline without breaking the bank</p>
         </div>
         <div class="service">
-            <img src="../Img_logo/seater-sofa.png" alt="Luxury and Comfort">
+            <img src="../image/seater-sofa.png" alt="Luxury and Comfort">
             <h3>Comfort Travel</h3>
-            <p>"Our airline offers our customers the opportunity to travel with comfort and ease, ensuring a seamless journey from takeoff to touchdown."</p>
+            <p>Our airline offers our customers the opportunity to travel with comfort and ease, ensuring a seamless journey from takeoff to touchdown.</p>
         </div>
 
         <div class="service">
-            <img src="../Img_logo/delivery.png" alt="Save and Secure">
+            <img src="../image/delivery.png" alt="Save and Secure">
             <h3>Easy Transport</h3>
-            <p>"Experience hassle-free travel with Easy Transport, where our airline provides customers with the opportunity to journey effortlessly and comfortably."</p>
+            <p>Experience hassle-free travel with Easy Transport, where our airline provides customers with the opportunity to journey effortlessly and comfortably.</p>
         </div>
 
         <div class="service">
-            <img src="../Img_logo/fast-service.png" alt="All Over The World">
+            <img src="../image/fast-service.png" alt="All Over The World">
             <h3>Fast Service</h3>
-            <p>"Experience swift and efficient service with our airline, where your time is valued and every aspect of your journey is handled promptly and seamlessly."</p>
+            <p>Experience swift and efficient service with our airline, where your time is valued and every aspect of your journey is handled promptly and seamlessly.</p>
         </div>
     </div>
 </section>
@@ -221,28 +226,22 @@
       <div class="text">+ Win Awards</div>
     </div>
   </section>
+  
   <footer class="footer">
     <div class="footer-content">
       <div class="footer-section about">
-        <img src="../Img_logo/aircraft__1_-removebg-preview.png" alt="Aircraft Image">
-        <p>With STAFFS_AIRWAYS, you can easily book any ticket you need to travel safely
-          thanks to our detailed system of searching and booking airline tickets.</p>
+        <img src="../image/aircraft-removebg-preview.png" alt="Aircraft Image">
+        <p>With STAFFS_AIRWAYS, you can easily book any ticket you need to travel safely thanks to our detailed system of searching and booking airline tickets.</p>
         <div class="contact">
           <span><i class="fas fa-phone"></i> +33 234 567 890</span>
           <span><i class="fas fa-envelope"></i> sttaffsairways@gmail.com</span>
-        </div>
-        <div class="social">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-linkedin"></i></a>
         </div>
       </div>
       <div class="footer-section links">
         <h2>Quick Links</h2>
         <ul>
-          <li><a href="../html/home.html">Home</a></li>
-          <li><a href="#">About</a></li>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="aboutUs.php">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
@@ -260,23 +259,9 @@
       </div>
     </div>
     <div class="footer-bottom">
-      &copy; 2024 Airline Management System | Designed by Your Name
+      &copy; 2024 Airline Management System | Designed by Nesrine - Caleb - Walid - Ulrich - Walker
     </div>
-  </footer>
-  <style>
-    .statistics {
-      display: flex;
-      justify-content: space-around;
-      padding: 50px 0; /* Ajuster le padding selon vos préférences */
-      background-image: url('../Img/jet.jpg'); /* Ajouter votre image de fond */
-      background-size: cover; /* Redimensionne l'image pour couvrir toute la section */
-      background-position: center; /* Centre l'image de fond */
-    }
-            
-    .statistic {
-      text-align: center;
-    }
-    </style>
+</footer>
 
 
 
